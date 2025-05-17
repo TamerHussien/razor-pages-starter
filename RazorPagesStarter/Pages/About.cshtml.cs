6bff448e8d03b3916e6bcea96785b75acfe14e5a
Tamer Hussien <eng.tamerhussien@gmail.com>
2025-05-17 12:31:32 +0000

    Add About page and update layout for navigation links

---
+++ RazorPagesStarter/Pages/About.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesStarter.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
