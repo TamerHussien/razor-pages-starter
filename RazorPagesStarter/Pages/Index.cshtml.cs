53bb8072c338e9e6a996ad30316d295e9de83b41
Tamer Hussien <eng.tamerhussien@gmail.com>
2025-05-17 12:19:47 +0000

    Refactor Index page to enhance form validation and layout structure

--- RazorPagesStarter/Pages/Index.cshtml.cs
+++ RazorPagesStarter/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesStarter.Pages;
 
 public class IndexModel : PageModel
 {
     [BindProperty]
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(50,MinimumLength = 2 ,ErrorMessage = "Name must be 2-50 characters.")]
     public string? UserName { get; set; }
 
     public string? Greeting { get; set; }
 
     public void OnPost()
     {
-        if (!string.IsNullOrWhiteSpace(UserName))
+        if (!ModelState.IsValid)
         {
-            Greeting = $"Hello, {UserName}!";
+            return;
         }
+
+        Greeting = $"Hello, {UserName}!";
     }
 }
